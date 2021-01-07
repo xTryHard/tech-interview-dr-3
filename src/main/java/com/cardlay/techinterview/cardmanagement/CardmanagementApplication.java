8fde2035611b7f66b6efad8ea9cf2da0326a2962
Magnus Biolzi Hansen <mbh@cardlay.com>
2021-01-07 23:38:15 +0100

    Initial commit

---
+++ src/main/java/com/cardlay/techinterview/cardmanagement/CardmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.cardlay.techinterview.cardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cardlay/techinterview/cardmanagement/CardmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardlay.techinterview.cardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
